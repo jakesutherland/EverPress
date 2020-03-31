2b6c7e8d320e26f1f761c25979349d765dd42fa3
Jake Sutherland <jake@sproutmedialab.com>
2020-03-30 20:31:31 -0400

    Added basic loop

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+get_header();
+?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-12">
+			<?php if ( have_posts() ) : ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<h1><?php the_title(); ?></h1>
+					<?php the_content(); ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
+	</div>
+</div>
+
+<?php
+get_footer();
+?>
